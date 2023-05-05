87100882266fdf8bf767c5342e8b41a698a88f02
Misha Sutiahin <51472670+Yahiko615@users.noreply.github.com>
2023-05-05 12:16:18 +0300

    finished task 1

---
+++ task1.py
@@ -0,0 +1,6 @@
+eleks_employees = ['Misha', 'Dima', 'Marina']
+eleks = ('eleks', eleks_employees)
+toshiba_employees = ['Alex', 'Den', 'Marina']
+toshiba = ('toshiba', toshiba_employees)
+toshiba_employees += eleks_employees
+print(toshiba_employees)
