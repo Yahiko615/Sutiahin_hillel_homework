237045fed8af5c62cd7cdd17cf60626c871d6bde
Misha Sutiahin <51472670+Yahiko615@users.noreply.github.com>
2023-05-20 18:57:29 +0300

    homework 6 task 8 done

---
+++ homework6/task8.py
@@ -0,0 +1,9 @@
+def display_box(width: int, height: int, character="x"):
+    box = f"{character * width}\n" + \
+          f"{character}{(width - 2) * ' '}{character}\n" * (height - 2) + \
+          f"{character * width}"
+    print(box)
+
+
+if __name__ == "__main__":
+    display_box(4, 4)
